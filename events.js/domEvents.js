24692d0fc9bc93b4d7ea23b2896d9d0b9a0545f6
Maddi Cook <maddihcook@gmail.com>
2023-03-07 19:37:34 -0600

    got the cards on the dom

--- events.js/domEvents.js
+++ events.js/domEvents.js
@@ -1,5 +1,16 @@
-// const domEvents = (user) => {
-//   document.querySelector('#main-container').addEventListener('click', (e) => {
+import { getVocab, htmlVocab } from '../api/getVocab';
+import { showVocab } from '../pages/vocab';
 
-//   });
-// };
+const domEvents = () => {
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+    if (e.target.id.includes('all')) {
+      getVocab().then(showVocab);
+    }
+
+    if (e.target.id.includes('html')) {
+      htmlVocab();
+    }
+  });
+};
+
+export default domEvents;
---
+++ pages/vocab.js
@@ -0,0 +1,30 @@
+import clearDom from '../utils/clearDom';
+import renderToDOM from '../utils/renderToDom';
+
+const emptyVocab = () => {
+  const domString = '<h1>No Vocab Words</h1>';
+  renderToDOM('#store', domString);
+};
+
+const showVocab = (array) => {
+  clearDom();
+
+  const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-vocab-btn">Add A Word</button>';
+  renderToDOM('#add-button', btnString);
+
+  let domString = '';
+  array.forEach((item) => {
+    domString += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.word}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${item.language}</h6>
+      <p class="card-text">${item.definition}</p>
+      <a href="#" class="edit-card-btn--${item.firebaseKey}">Edit</a>
+      <a href="#" class="delete-card-btn--${item.firebaseKey}">Delete</a>
+    </div>
+  </div>`;
+  });
+  renderToDOM('#store', domString);
+};
+
+export { emptyVocab, showVocab };
